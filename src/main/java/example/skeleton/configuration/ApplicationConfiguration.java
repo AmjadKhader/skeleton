80927ef635c5aaab9e112d439133284cab2b83cc
Amjad Khader <amjad.khader@mawdoo3.com>
2023-02-21 09:38:35 +0200

    Update checking authentication

--- src/main/java/example/skeleton/configuration/ApplicationConfiguration.java
+++ src/main/java/example/skeleton/configuration/ApplicationConfiguration.java
@@ -14,5 +14,5 @@ import java.util.List;
 @Component
 @ConfigurationProperties(prefix = "security")
 public class ApplicationConfiguration {
-    private List<AuthenticationEndpointConfiguration> checkAuthentication;
+    private List<AuthenticationEndpointConfiguration> skipCheckAuthentication;
 }
